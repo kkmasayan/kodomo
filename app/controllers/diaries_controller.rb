f6121b76a9a5f4038e51e1cfa39647dc51a89a73
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2022-12-18 21:54:36 +0900

    日記編集機能済

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,5 +1,7 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
+  before_action :set_diary, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
   def index
     @diaries = Diary.order('created_at DESC')
   end
@@ -16,12 +18,27 @@ class DiariesController < ApplicationController
   end
 
   def show
-    @diary = Diary.find(params[:id])
+  end
+  
+  def edit
   end
 
+  def update
+    if @diary.update(diary_params)
+      redirect_to diary_path
+    else
+      render :edit
+    end
+  end
   private
 
   def diary_params
     params.require(:diary).permit(:title, :text, :category_id, :image).merge(user_id: current_user.id)
+  end
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
+  def move_to_index
+    redirect_to action: :index unless current_user.id == @diary.user_id
   end
 end
