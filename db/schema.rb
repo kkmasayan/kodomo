10d6e0efe4b366f59b03c8eb07f301877f7e2a33
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2023-01-07 23:25:41 +0900

    マイグレーションリセット

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(version: 2022_12_21_134903) do
     t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "diary_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["diary_id"], name: "index_favorites_on_diary_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,4 +88,6 @@ ActiveRecord::Schema.define(version: 2022_12_21_134903) do
   add_foreign_key "comments", "diaries"
   add_foreign_key "comments", "users"
   add_foreign_key "diaries", "users"
+  add_foreign_key "favorites", "diaries"
+  add_foreign_key "favorites", "users"
 end
