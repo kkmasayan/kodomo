579841fd5006912be5fc4abdd0200e15c670d5fd
kkmasayan <108529674+kkmasayan@users.noreply.github.com>
2023-01-20 21:08:29 +0900

    マイページ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @diaries = user.diaries
-    @diary = Diary.find(params[:id])
+    
     favorites = Favorite.where(user_id: current_user.id).pluck(:diary_id)  # ログイン中のユーザーのお気に入りのdiary_idカラムを取得
     @favorite_list = Diary.find(favorites)     # diariesテーブルから、お気に入り登録済みのレコードを取得
   end
